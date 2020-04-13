7c554aafa4d4561e44a340ed7c8e046ba894f916
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-04-13 15:10:39 +0800

    我的代码作业

---
+++ bin/tutor_var.dart
@@ -0,0 +1,11 @@
+import 'package:assignment1/assignment1.dart' as assignment1;
+
+main(List<String> arguments) {
+  var name="sean";
+  String nameStr="seanStr";
+  dynamic nameDy="seanDy";
+  print(name);
+  print(nameStr);
+  print(nameDy);
+  print('Hello world: ${assignment1.calculate()}!');
+}
