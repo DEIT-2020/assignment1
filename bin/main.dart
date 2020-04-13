856c36d190c84d1220376f4b1266aa1ab350cc7d
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-04-13 15:15:42 +0800

    我的代码作业

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,5 @@
 import 'package:assignment1/assignment1.dart' as assignment1;
 
 main(List<String> arguments) {
-  print('Hello world: ${assignment1.calculate()}!');
+  print('Hello world!!: ${assignment1.calculate()}!');
 }
