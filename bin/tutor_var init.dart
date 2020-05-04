eaefa8c9914a7a03cf0b8cea3ac4e87ce767d147
cuixueli1 <472542789@qq.com>
2020-05-04 23:00:13 +0800

    我的作业代码

---
+++ bin/tutor_var init.dart
@@ -0,0 +1,8 @@
+import 'package:assignment1/assignment1.dart' as assignment1;
+
+main(List<String> arguments) {
+  var name="name";
+  String nameStr ;
+  dynamic nameDy;
+  assert(name == null);
+}
